c6b447d3c565dd4c32926408dec0087d1cb0626b
borji moatez <123332288+borgimoatez123@users.noreply.github.com>
2024-09-28 11:49:38 +0100

    Update ZooManagement.java

--- prosit1/ZooManagement.java
+++ prosit1/ZooManagement.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages;
+    String ZooName;
+
+    public static void main(String[] args) {
+        ZooManagement zooManagement = new ZooManagement();
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the name of zoo");
+        zooManagement.ZooName = input.nextLine();
+        System.out.println("enter the number of cages");
+        zooManagement.nbrCages = input.nextInt();
+        System.out.println(zooManagement.ZooName+" comporte "+zooManagement.nbrCages+"cages");
+
+    }
+}
